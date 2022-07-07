23c7cc35cf19f5cbff7c2c82c92031cd5e60f0c9
alexanderjmat <alexanderjmatula@gmail.com>
2022-07-07 17:07:52 -0400

    progress

---
+++ js/stories.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+
+/** Get and show stories when site first loads. */
+const formSubmit = $('#form')
+
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+/**
+ * A render method to render HTML for an individual Story instance
+ * - story: an instance of Story
+ *
+ * Returns the markup for the story.
+ */
+
+function generateStoryMarkup(story) {
+  // console.debug("generateStoryMarkup", story);
+
+  const hostName = story.getHostName();
+  return $(`
+      <li id="${story.storyId}">
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    `);
+}
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+
+  // loop through all of our stories and generate HTML for them
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  }
+
+  $allStoriesList.show();
+}
+
+formSubmit.on('submit', submitStories)
+
+async function submitStories() {
+  const auth = $('#author')[0];
+  const title = $('#title')[0];
+  const url = $('#url')[0];
+  await StoryList.addStory(currentUser, {title: title.value, author: author.value, url: `https://${url.value}`});
+  getAndShowStoriesOnStart();
+}
+
+async function test() { 
+  let newStory = await StoryList.addStory(currentUser,
+    {title: "Test", author: "Me", url: "http://meow.com"});
+
+}
+
